26c2420de8108b1a8fd89e43f7c59bd3cdabc129
Michael Simon <simon@kit.edu>
2017-07-20 09:03:46 +0200

    No n:m between user and account, just 1:n

--- bwreg-jpa/src/main/java/edu/kit/scc/webreg/entity/account/AccountEntity.java
+++ bwreg-jpa/src/main/java/edu/kit/scc/webreg/entity/account/AccountEntity.java
@@ -5,13 +5,17 @@ import java.util.Map;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
 import javax.persistence.MapKeyColumn;
 import javax.persistence.Table;
 
 import edu.kit.scc.webreg.entity.AbstractBaseEntity;
+import edu.kit.scc.webreg.entity.UserEntity;
 
 @Entity(name = "AccountEntity")
 @Table(name = "account")
@@ -20,6 +24,11 @@ public class AccountEntity extends AbstractBaseEntity {
 
 	private static final long serialVersionUID = 1L;
 
+    @Id
+	@ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+	private UserEntity user;
+	
 	@ElementCollection
 	@JoinTable(name = "account_attribute_store")
     @MapKeyColumn(name = "key_data", length = 1024)
@@ -32,5 +41,13 @@ public class AccountEntity extends AbstractBaseEntity {
 
 	public void setAccountStore(Map<String, String> accountStore) {
 		this.accountStore = accountStore;
+	}
+
+	public UserEntity getUser() {
+		return user;
+	}
+
+	public void setUser(UserEntity user) {
+		this.user = user;
 	} 
 }
