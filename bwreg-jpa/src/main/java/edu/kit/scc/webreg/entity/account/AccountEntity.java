2f2bb004a6166685cccd115e1519b898b04269bf
Michael Simon <simon@kit.edu>
2017-07-20 07:08:31 +0200

    Introduce generic Account store for attribute

--- bwreg-jpa/src/main/java/edu/kit/scc/webreg/entity/account/AccountEntity.java
+++ bwreg-jpa/src/main/java/edu/kit/scc/webreg/entity/account/AccountEntity.java
@@ -1,8 +1,14 @@
 package edu.kit.scc.webreg.entity.account;
 
+import java.util.Map;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.JoinTable;
+import javax.persistence.MapKeyColumn;
 import javax.persistence.Table;
 
 import edu.kit.scc.webreg.entity.AbstractBaseEntity;
@@ -14,5 +20,17 @@ public class AccountEntity extends AbstractBaseEntity {
 
 	private static final long serialVersionUID = 1L;
 
+	@ElementCollection
+	@JoinTable(name = "account_attribute_store")
+    @MapKeyColumn(name = "key_data", length = 1024)
+    @Column(name = "value_data", length = 2048)
+    private Map<String, String> accountStore;
 
+	public Map<String, String> getAccountStore() {
+		return accountStore;
+	}
+
+	public void setAccountStore(Map<String, String> accountStore) {
+		this.accountStore = accountStore;
+	} 
 }
