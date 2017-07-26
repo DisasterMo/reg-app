a783f42ec70f81a6365fc1ca59da01ab77ca104c
Michael Simon <simon@kit.edu>
2017-07-26 14:48:58 +0200

    Delete a lot of files, just to rebuild them anyways

--- regapp-service/src/main/java/edu/kit/scc/webreg/approval/AbstractApprovalWorkflow.java
+++
@@ -1,8 +0,0 @@
-package edu.kit.scc.webreg.approval;
-
-import edu.kit.scc.webreg.service.reg.ApprovalWorkflow;
-
-public abstract class AbstractApprovalWorkflow implements ApprovalWorkflow {
-
-
-}
--- regapp-service/src/main/java/edu/kit/scc/webreg/dto/entity/BaseEntityDto.java
+++
@@ -1,8 +0,0 @@
-package edu.kit.scc.webreg.dto.entity;
-
-import java.io.Serializable;
-
-public interface BaseEntityDto<PK extends Serializable> {
-	PK getId();
-	void setId(PK id);
-}
