a6e9683224d903681ea666c8b6517f1ff6f1c4fa
Michael Simon <simon@kit.edu>
2017-08-31 11:05:47 +0200

    Angular first steps

--- reg-app-frontend/src/app/app.module.ts
+++ reg-app-frontend/src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'index', component: IndexComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/index', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      // { enableTracing: true } <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ reg-app-frontend/src/app/index/index.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'index-index',
+    templateUrl: './index.component.html',
+    styleUrls: ['./index.component.css']
+  })
+  export class IndexComponent {
+  }
