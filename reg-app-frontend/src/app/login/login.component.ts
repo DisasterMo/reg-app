d7e20b48761a281dd89c1cc697d7ab729546e903
Michael Simon <simon@kit.edu>
2017-09-04 07:07:28 +0200

    show authentication mechs

--- reg-app-frontend/src/app/login/login.component.ts
+++ reg-app-frontend/src/app/login/login.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../login/login.service';
+import { AuthMech } from '../data/auth-mech';
 
 @Component({
     selector: 'app-login-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.css']
-  })
-  export class LoginComponent {
+})
+export class LoginComponent implements OnInit {
+
+  authMechs: AuthMech[];
+
+  constructor(private loginService: LoginService) { }
+
+  getAuthMechs(): void {
+    this.loginService.getAuthMechList().then(authMechs => this.authMechs = authMechs);
   }
+
+  ngOnInit(): void {
+    this.getAuthMechs();
+  }
+}
--- reg-app-frontend/src/app/login/login.service.ts
+++ reg-app-frontend/src/app/login/login.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { User } from '../data/user';
 import { AuthInfo } from '../data/auth-info';
+import { AuthMech } from '../data/auth-mech';
 import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
@@ -12,6 +13,7 @@ import 'rxjs/add/operator/catch';
 export class LoginService {
 
     private authUrl = '/rest/auth/info';
+    private authMechUrl = '/rest/auth/mech/list';
     private userUrl = '/rest/user/detail';
 
     constructor(private http: Http) { }
@@ -23,10 +25,17 @@ export class LoginService {
             .catch(error => this.handleError(error));
     }
 
+    getAuthMechList(): Promise<AuthMech[]> {
+        return this.http.get(this.authMechUrl)
+            .map((res: Response) => res.json())
+            .toPromise()
+            .catch(error => this.handleError(error));
+    }
+
     getUser(): Observable<User> {
         return this.http.get(this.userUrl)
             .map((res: Response) => res.json())
-            .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+            .catch(error => this.handleError(error));
     }
 
     private handleError(error: any): Promise<any> {
